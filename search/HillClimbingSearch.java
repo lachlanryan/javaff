011067eda2184eecfc7391fe40ee836bb1744bce
lachlanryan <lachlanryan90@gmail.com>
2014-10-16 21:14:47 +1000

    Update HillClimbingSearch.java

--- search/HillClimbingSearch.java
+++ search/HillClimbingSearch.java
@@ -65,35 +65,53 @@ public class HillClimbingSearch extends Search
 		needToVisit(start); // dummy call (adds start to the list of 'closed' states so we don't visit it again
 		
 		open.add(start); // add it to the open list
-		bestHValue = start.getHValue(); // and take its heuristic value as the best so far
+		
 
-		javaff.JavaFF.infoOutput.println(bestHValue);
+		
 		
 		while (!open.isEmpty()) // whilst still states to consider
 		{	
+		
 			State s = removeNext(); // get the next one
 			
 			Set successors = s.getNextStates(filter.getActions(s)); // and find its neighbourhood
 			
-			Iterator succItr = successors.iterator();			
-		
+			Iterator succItr = successors.iterator();
+			bestHValue = start.getHValue(); // and take its heuristic value as the best so far	
+					
+			javaff.JavaFF.infoOutput.println(bestHValue);
+			
 			while (succItr.hasNext()) {
 				State succ = (State) succItr.next(); // next successor
+				
+				
 			
 				if (needToVisit(succ)) {
 					if (succ.goalReached()) { // if we've found a goal state - return it as the solution
 						return succ;
 					} else if (succ.getHValue().compareTo(bestHValue) < 0) {
 						// if we've found a state with a better heuristic value than the best seen so far
-					
-						bestHValue = succ.getHValue(); // note the new best avlue
+						
+						bestHValue = succ.getHValue(); // note the new best value
 						javaff.JavaFF.infoOutput.println(bestHValue);
-						open = new LinkedList(); // clear the open list
-						open.add(succ); // put this on it
-						break; // and skip looking at the other successors
+						bestSuccessor = new LinkedList();
+						bestSuccessor.add(succ);
 					} else {
-						open.add(succ); // otherwise, add to the open list
+						bestSucessor.add(succ); // otherwise, add to the open list
 					}
+				if (bestSuccessor = null){
+				open = new LinkedList();
+				}else{
+				int tempNum = javaff.JavaFF.generator.nextInt(bestsuccessor.size());
+				State = temp = (State) bestSuccessor.get(tempNum);
+				open = new LinkedList();
+				
+				open.add(temp);				
+				
+				
+				}
+				
+				
 				}
 			}
 			
